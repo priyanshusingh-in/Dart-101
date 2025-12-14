3eae82aa7ce25b6968b1b9eb17bbd34fdd7845ca
priyanshusingh-in <in.priyanshusingh@gmail.com>
2025-12-14 16:42:06 +0530

    Add explicit type annotations and null safety example in annotations.dart

---
+++ basic/annotations.dart
@@ -0,0 +1,23 @@
+// Explicit type annotations in Dart
+
+void main() {
+  String name = 'Mario';
+
+  print(name);
+
+  int age = 23;
+  print(age);
+
+  bool isOpen = false;
+  print(isOpen);
+
+  double averageRating = 4.3;
+  print(averageRating);
+
+  var fruit = ['apple', 'banana', 'orange'];
+  print(fruit);
+
+  //null safety in Dart
+  int? point;
+  print(point);
+}
