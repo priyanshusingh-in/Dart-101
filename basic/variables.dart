327ff4e3c3428da0f5e0d6d0d6ec72ded818e9c3
priyanshusingh-in <in.priyanshusingh@gmail.com>
2026-05-17 18:32:56 +0530

    Add initial variables example with registration number, name, and location

---
+++ basic/variables.dart
@@ -0,0 +1,9 @@
+void main() {
+  int RegNum = 12204367;
+  String name = "Priyanshu Singh";
+  dynamic location = "Sonbhadra, Uttar Pradesh";
+
+  print(
+    "My name is $name, my registration number is $RegNum and I am from $location.",
+  );
+}
